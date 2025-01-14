247e7718faf0e1278087405e84fd249274a616f3
YasinRabiee <hlww6303@gmail.com>
2025-01-14 04:47:56 -0800

    add Vazir font

--- themes/js/script.js
+++ themes/js/script.js
@@ -8,31 +8,59 @@ JSON.parse(localStorage.getItem(`todos`));
 let numberTodo = localStorage.getItem(`todos`) === null ? -1 :
 JSON.parse(localStorage.getItem(`todos`)).length;
 
+function addTodo() {
+	numberTodo++;
+	localStorage.setItem(`lastNumber`, numberTodo);
+	const text = input.val();
+	let dir = undefined;
+	if (checkDir(text))
+		dir = `ltr`;
+	else
+		dir = `rtl`;
+	input.val(``);
+	todo.append(`
+		<li class="items list-group-item" id="item-${numberTodo}"
+		style="background-color: ${bgc}; direction: ${dir};">
+			${text}
+		</li>
+	`);
+	input.focus();
+	let newObject = {
+		number: numberTodo,
+		text: text,
+		color: bgc,
+		direction: dir
+	}
+	todos.push(newObject);
+	localStorage.setItem(`todos`, JSON.stringify(todos));
+}
+
 function removeItem(ar, ele) {
     ar.forEach((item, index) => {
-        if (item.text == ele) {
+        if (item.number == ele) {
         	ar.splice(index, 1);
         }
     });
     return ar;
 }
 
+function checkDir(text) {
+	const ch = text.charCodeAt(0);
+	if (ch >= 65 && ch <= 122)
+		return true;
+	return false;
+}
+
 btn.click(function() {
-	numberTodo++;
-	localStorage.setItem(`lastNumber`, numberTodo);
-	const text = input.val();
-	input.val(``);
-	todo.append(`
-		<li class="items list-group-item" id="item-${numberTodo}" style="background-color: ${bgc}">${text}</li>
-	`);
-	input.focus();
-	let newObject = {
-		number: numberTodo,
-		text: text,
-		color: bgc
+	if (input.val() !== ``)
+		addTodo();
+});
+
+input.keydown(function(e) {
+	if (input.val() !== ``) {
+		if (e.key === `Enter`)
+			addTodo();
 	}
-	todos.push(newObject);
-	localStorage.setItem(`todos`, JSON.stringify(todos));
 });
 
 window.addEventListener(`load`, function(e) {
@@ -41,7 +69,9 @@ window.addEventListener(`load`, function(e) {
 		const l = lastTodos.length;
 		for (let i = 0; i < l; i++) {
 			todo.append(`
-				<li class="items list-group-item" id="item-${lastTodos[i].number}" style="background-color: ${lastTodos[i].color}">
+				<li class="items list-group-item" id="item-${lastTodos[i].number}"
+				style="background-color: ${lastTodos[i].color};
+				direction: ${lastTodos[i].direction};">
 					${lastTodos[i].text}
 				</li>
 			`);			
@@ -52,8 +82,10 @@ window.addEventListener(`load`, function(e) {
 todo.on('click', 'li', function() {
 	$(this).fadeOut();
 
-	let item = $(this).html();
-	item = item.replaceAll(/\s/g, ``);
+	// let item = $(this).html();
+	// item = item.replaceAll(/\s/g, ``);
+	let item = $(this).attr(`id`);
+	item = item.replaceAll(`item-`, ``);
 	todos = removeItem(todos, item);
 	localStorage.setItem(`todos`, JSON.stringify(todos));
 });
@@ -61,4 +93,11 @@ todo.on('click', 'li', function() {
 $(`.color`).click(function() {
 	bgc = $(this).css(`background-color`);
 	$(`#preview li`).css(`background-color`, bgc);
+});
+
+input.on(`input`, function() {
+	if (checkDir($(this).val()))
+		$(this).css(`direction`, `ltr`);
+	else
+		$(this).css(`direction`, `rtl`);
 });
